34f856fe899a9b8c6ddfe200fa9910874f7b6dbf
Eddie Leeper <eddie@notbinary.co.uk>
2020-03-28 12:49:45 +0000

    fixed strong migrations error

--- db/migrate/20200328115130_add_auth_details_to_users.rb
+++ db/migrate/20200328115130_add_auth_details_to_users.rb
@@ -1,7 +1,9 @@
 class AddAuthDetailsToUsers < ActiveRecord::Migration[6.0]
+  disable_ddl_transaction!
+
   def change
     add_column :users, :email, :string
-    add_index :users, :email, unique: true
+    add_index :users, :email, unique: true, algorithm: :concurrently
     add_column :users, :provider, :string
   end
 end
